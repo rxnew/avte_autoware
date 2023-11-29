035d12413971dc859ec1a0ee00bd689eff6700ca
rxnew <rxnew.axdseuan@gmail.com>
2023-11-29 22:29:31 +0900

    Fix .webauto-ci.yaml

--- .webauto-ci/scenario_simulator_v2/colcon-build/run.sh
+++ .webauto-ci/scenario_simulator_v2/colcon-build/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 : "${WEBAUTO_CI_SOURCE_PATH:?is not set}"
+: "${ROS_DISTRO:?is not set}"
+: "${WORKDIR:?is not set}"
 
-mkdir -p /home/ubuntu/Desktop/scenario_simulator_ws
-cd /home/ubuntu/Desktop/scenario_simulator_ws
+mkdir -p "$WORKDIR"
+cd "$WORKDIR"
 
 mkdir -p src
 cp -rfT "$WEBAUTO_CI_SOURCE_PATH"/src src
