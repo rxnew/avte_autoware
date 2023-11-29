f42f4169aba81e0281c5aa07ea90a70df0c944ea
rxnew <rxnew.axdseuan@gmail.com>
2023-11-29 22:02:46 +0900

    Add build phase for scenario_simulator_v2

---
+++ .webauto-ci/scenario_simulator_v2/colcon-build/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+: "${WEBAUTO_CI_SOURCE_PATH:?is not set}"
+
+mkdir -p src
+cp -rfT "$WEBAUTO_CI_SOURCE_PATH"/src src
+source /opt/ros/"$ROS_DISTRO"/setup.bash
+rosdep update
+rosdep install -iry --from-paths src
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
---
+++ .webauto-ci/scenario_simulator_v2/install-middleware/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+apt-get -y update
+apt-get -y install ros-humble-rmw-cyclonedds-cpp
