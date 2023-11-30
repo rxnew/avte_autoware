ae562217dff12863182c901776bab6cf56783aef
rxnew <rxnew.axdseuan@gmail.com>
2023-11-30 13:41:08 +0900

    Fix colcon-build script

--- .webauto-ci/scenario_simulator_v2/colcon-build/run.sh
+++ .webauto-ci/scenario_simulator_v2/colcon-build/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 : "${WEBAUTO_CI_SOURCE_PATH:?is not set}"
-: "${ROS_DISTRO:?is not set}"
 : "${WORKDIR:?is not set}"
 
 mkdir -p "$WORKDIR"
@@ -9,6 +8,8 @@ cd "$WORKDIR"
 
 mkdir -p src
 cp -rfT "$WEBAUTO_CI_SOURCE_PATH"/src src
+# shellcheck disable=SC2012
+ROS_DISTRO=$(ls -1 /opt/ros | head -1)
 source /opt/ros/"$ROS_DISTRO"/setup.bash
 rosdep update
 rosdep install -iry --from-paths src
